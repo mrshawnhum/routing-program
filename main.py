a5e2a744acff42b67741559464a2fa05c005f024
Shawn <shawnhum97@gmail.com>
2025-04-29 02:06:22 -0500

    Test hash table

---
+++ main.py
@@ -0,0 +1,31 @@
+from CreateHashTable import CreateHashTable
+
+def main():
+    bootStrap = [
+        [1, "test1"],
+        [2, "test2"],
+        [3, "test3"],
+        [4, "test4"],
+        [5, "test5"],
+    ]
+
+    testTable = CreateHashTable()
+
+    print("<---- testing bootstrap --->")
+    testTable.insert(bootStrap[0][0], bootStrap[0][1])
+    print(testTable.table)
+
+    testTable.insert(bootStrap[1][0], bootStrap[1][1])
+
+    print("\nSearch")
+    print(testTable.search(1))
+    print(testTable.search(2))
+    print(testTable.search(11))
+
+    print("\nUpdate")
+    testTable.insert(1, "new tester")
+    print(testTable.table)
+
+
+if __name__ == "__main__":
+    main()
