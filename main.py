dd205f500edd880bb011509202b2878d95c1223f
Shawn <shawnhum97@gmail.com>
2025-04-30 05:09:04 -0500

    Add package data table logic

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
+# Class file imports
 from CreateHashTable import CreateHashTable
+from Package import Package
+# imports
+import csv
+from datetime import datetime
+
+def load_packages(filename="WGUPS-data.csv"):
+    package_table = CreateHashTable(initial_capacity=40)
+    test_all_packages = []
+
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            # unpack data
+            pkg_ID, address, city, state, zip_code, deadline, pkg_weight = row
+
+            # convert type
+            ID = int(pkg_ID)
+            weight = int(pkg_weight)
+
+            # defaults
+            status = "At Hub"
+
+            # build Package objects
+            package = Package(ID, address.strip(), city.strip(), state.strip(), zip_code.strip(), deadline, weight, status)
+
+            package_table.insert(ID, package)
+            test_all_packages.append(package)
+
+    return package_table, test_all_packages
+
+
 
 def main():
+
+    table, test_pkg_list = load_packages()
+
+    print("All packages")
+    for pkg in test_pkg_list:
+        print(pkg.ID, pkg.status)
+    '''
     bootStrap = [
         [1, "test1"],
         [2, "test2"],
@@ -29,7 +68,7 @@ def main():
     print("\nRemove")
     testTable.remove(1)
     print(testTable.table)
-
+    '''
 
 if __name__ == "__main__":
     main()
