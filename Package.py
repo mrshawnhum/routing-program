af31df85f23349f25a25748bf1d181089450121c
Shawn <shawnhum97@gmail.com>
2025-04-30 05:08:22 -0500

    Add string function to package class

--- Package.py
+++ Package.py
@@ -1,12 +1,17 @@
 # Object class for packages
 class Package:
-    def __init__(self, packageID, address, deadline, city, zipCode, weight, status):
-        self.packageID = packageID
+    def __init__(self, ID, address, city, state, zip_code, deadline, weight, status):
+        self.ID = ID
         self.address = address
-        self.deadline = deadline
         self.city = city
-        self.zipCode = zipCode
+        self.state = state
+        self.zip_code = zip_code
+        self.deadline = deadline
         self.weight = weight
         self.status = status
-        self.departureTime = None
-        self.arrivalTime = None
+        self.departure_time = None
+        self.arrival_time = None
+
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s" % (self.ID, self.address, self.city, self.state, self.zip_code, self.weight,
+                                               self.status)
