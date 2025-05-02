b01b119e0eedc7926e12d1195146fd26996d55eb
Shawn <shawnhum97@gmail.com>
2025-05-02 00:55:30 -0500

    Adjust Truck class variables

--- Truck.py
+++ Truck.py
@@ -1,14 +1,13 @@
 # Object class for trucks
 class Truck():
-    def __init__(self, capacity, speed, load, packages, mileage, address, departure_time):
+    def __init__(self, capacity, speed, load, mileage, current_address, departure_time):
         self.capacity = capacity
         self.speed = speed
         self.load = load
-        self.packages = packages
         self.mileage = mileage
-        self.address = address
+        self.current_address = current_address
         self.departure_time = departure_time
 
     def __str__(self):
-        return "%s %s %s %s %s %s %s" % (self.capacity, self.speed, self.load, self.packages, self.mileage,
-                                         self.departure_time, self.address)
+        return "%s %s %s %s %s %s" % (self.capacity, self.speed, self.load, self.mileage,
+                                         self.departure_time, self.current_address)
